51080cd216dd1136c60827a3b26069bd11b2aa76
Adrian Sapien <Asapie269@west-mec.local>
2021-08-30 15:03:50 -0700

    added logo to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Home/>
-=======
       <Navbar />
->>>>>>> 4c716365a81a3e327bf1b3ae2676ce7114fb37b1
+      <Home />
     </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className='navbar'>
             <div className="logo">
-                <img src="#" alt="Logo" />
+                
             </div>
             <div className="navbar-content">
                 <ul>
